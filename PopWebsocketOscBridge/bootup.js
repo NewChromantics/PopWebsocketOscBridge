bd428b70ad69ab4bfc771455d0ee342ad9b810ad
Graham Reeves <graham@grahamreeves.com>
2020-05-13 16:08:52 +0100

    Added OSC message sending

--- PopWebsocketOscBridge/bootup.js
+++ PopWebsocketOscBridge/bootup.js
@@ -47,12 +47,119 @@ async function WaitForWebsocketToOscMessage()
 	return WebsocketToOscQueue.Allocate();
 }
 
+function EncodeOscMessage(Address,Floats,Ints)
+{
+	let Packet = [];
+	
+	function Pad()
+	{
+		//	OSC packets need to be multiples of 32bit
+		while( (Packet.length % 4 ) != 0 )
+			Packet.push(0);
+	}
+	
+	function Finish()
+	{
+		//	seems Protokol (not sure about spec) needs at least one terminator
+		if ( Packet[Packet.length-1] != 0 )
+			Packet.push(0);
+		
+		Pad();
+	}
+	
+	function AppendString(Str)
+	{
+		for ( let i = 0; i < Str.length; i++)
+		{
+			const Char = Str.charCodeAt(i);
+			Packet.push(Char);
+		}
+		Packet.push(0);
+		Pad();
+	}
+	
+	function AppendFloat(Float)
+	{
+		const Arrayf = new Float32Array(1);
+		Arrayf[0] = Float;
+		const Array8 = new Uint8Array(Arrayf.buffer);
+		Packet.push(Array8[3]);
+		Packet.push(Array8[2]);
+		Packet.push(Array8[1]);
+		Packet.push(Array8[0]);
+		Pad();
+	}
+	
+	function AppendInt(Float)
+	{
+		const Arrayf = new Int32Array(1);
+		Arrayf[0] = Float;
+		const Array8 = new Uint8Array(Arrayf.buffer);
+		Packet.push(Array8[3]);
+		Packet.push(Array8[2]);
+		Packet.push(Array8[1]);
+		Packet.push(Array8[0]);
+		Pad();
+	}
+	
+	//	turn this input into an OSC packet
+	if ( Address[0] != '/' )
+		throw `OSC address doesn't start with /: ${Address}`;
+	AppendString(Address);
+	
+	Floats = Floats || [];
+	Ints = Ints || [];
+
+	if ( Floats.length || Ints.length )
+	{
+		let Types = ',';
+		for ( let i=0;	i<Floats.length;	i++ )
+			Types += 'f';
+		for ( let i=0;	i<Ints.length;	i++ )
+			Types += 'i';
+		AppendString(Types);
+		
+		for ( let i=0;	i<Floats.length;	i++ )
+			AppendFloat(Floats[i]);
+		for ( let i=0;	i<Ints.length;	i++ )
+			AppendInt(Ints[i]);
+	}
+	
+	/*
+	 AppendString(',i');
+	 Packet.push(0);
+	 Packet.push(0);
+	 Packet.push(1);
+	 Packet.push(1);
+	 */
+	//AppendString(',f');
+	//AppendFloat(123.45);
+	
+	Finish();
+	//	put in queue
+	Packet = new Uint8Array(Packet);
+	return Packet;
+}
+
 function SendWebsocketToOscMessage(Message)
 {
-	Pop.Debug(`Websocket: ${Message}`);
-	//	put in queue
-	WebsocketToOscQueue.Resolve(Message);
+	const Packet = EncodeOscMessage('/foo',[123.45],[1,2,3]);
+	WebsocketToOscQueue.Resolve(Packet);
 }
+
+async function FloodTest()
+{
+	while (true)
+	{
+		await Pop.Yield(1000);
+		const Osc = {};
+		Osc.Address = '/hello';
+		Osc.Value = '1';
+		SendWebsocketToOscMessage(Osc);
+	}
+}
+FloodTest().then().catch(Pop.Debug);
+				
 
 function SendOscToWebsocketMessage(Message)
 {
@@ -114,30 +221,40 @@ async function UdpClientLoop(GetConnectAddress,OnRecvMessage,WaitForSendMessage)
 		const Address = GetConnectAddress();
 		try
 		{
-			const Client = new Pop.Socket.UdpClient(...Address);
-			await Client.WaitForConnect();
-			Pop.Debug(`Udp connected ${Address}; from`, JSON.stringify(Client.GetAddress()));
+			const Socket = new Pop.Socket.UdpClient(...Address);
+			await Socket.WaitForConnect();
+			Pop.Debug(`Udp connected ${Address}; from`, JSON.stringify(Socket.GetAddress()));
 			
 			const SendLoop = async function()
 			{
 				while ( true )
 				{
 					const Message = await WaitForSendMessage();
-					const Peers = Server.GetPeers();
+					const Peers = Socket.GetPeers();
+					Pop.Debug(`Sending ${Message} to Peers ${Peers}`);
 					function Send(Peer)
 					{
-						Server.Send( Peer, Message );
+						try
+						{
+							const Type = typeof Message;
+							Pop.Debug(`Sending ${Message}(${Type}) to ${Peer}`);
+							Socket.Send( Peer, Message );
+						}
+						catch(e)
+						{
+							Pop.Debug(`Send error ${e}`);
+						}
 					}
 					Peers.forEach(Send);
 				}
 			}
 			let SendError = null;
 			//	start the send loop
-			SendLoop().then().catch( e => SendError = e );
+			SendLoop().then().catch(Pop.Debug);// e => SendError = e );
 			
 			while ( true )
 			{
-				const Message = await Client.WaitForMessage();
+				const Message = await Socket.WaitForMessage();
 				OnRecvMessage(Message);
 				
 				if ( SendError )
@@ -159,7 +276,7 @@ function GetNextWebsocketPort()
 
 function GetNextUdpAddress()
 {
-	return ['localhost',63803];
+	return ['localhost',9999];//63803];
 }
 
 
